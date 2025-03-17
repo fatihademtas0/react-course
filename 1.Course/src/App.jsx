028ede9761ddbe8d2bace296652de422fe7e3148
fatihademtas0 <fatihademtas@outlook.com>
2025-03-17 20:40:05 +0300

    first commit

---
+++ 1.Course/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let a = 15;
+
+  const firstName = 'Esra';
+
+  let names = [
+    'Esra',
+    'Fatih',
+    'Osman',
+    'Mehmet'
+  ];
+
+  return (
+    <div>
+      <p>Value of a:  {a}</p>
+      <p>Customers Name:  {firstName}</p>
+
+
+      {
+        names.map((name , index)=>(
+          <div key={index} style={{
+            backgroundColor: 'green',
+            border: '1px solid black'
+
+          }}>
+            {index+1} : {name}
+          </div>   
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
