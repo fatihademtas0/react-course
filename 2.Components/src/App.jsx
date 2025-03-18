13913caaebb5e93ad498848af6310156b72a554e
fatihademtas0 <fatihademtas@outlook.com>
2025-03-18 22:10:30 +0300

    js and jsx diffs

--- 2.Components/src/App.jsx
+++ 2.Components/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Login'
+import Hello from './Hello'
 
 function App() {
 
   return (
     <div>
       <Login />
+      <Hello />
     </div>
   )
 }
---
+++ 2.Components/src/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hello() {
+  return React.createElement("div", null, "Hello");
+}
+
+export default Hello;
