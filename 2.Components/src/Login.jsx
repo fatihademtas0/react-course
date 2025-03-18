1a1f38f5f64ffac1d72b493b089e7f7f34a11d4b
fatihademtas0 <fatihademtas@outlook.com>
2025-03-18 22:03:50 +0300

    login

---
+++ 2.Components/src/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+      <div>
+
+        <div>
+          <p>Username</p>
+          <input type="text" />
+        </div>
+        
+        <div>
+          <p>Password</p>
+          <input type="text" />
+        </div>
+      
+        <button>Login</button>
+
+      </div>
+    </>
+  )
+}
+
+export default Login
