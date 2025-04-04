55f270a8c72d6f84acb382c430fc4a58d5c91b64
fatihademtas0 <fatihademtas@outlook.com>
2025-04-05 01:38:27 +0300

    custom hooks

---
+++ 15.Custom-Hooks/src/hooks/useCounter.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+function useCounter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return {
+        count,
+        increment,
+        decrement,
+    };
+}
+
+export default useCounter;
